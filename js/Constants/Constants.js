c4b4835ecd34b0986b12c02652bb7b114f2ce6bb
Hayer, Jaspal (Software Developer) <jaspal.hayer@sky.uk>
2018-01-05 09:06:38 +0000

    fucked the stash

---
+++ js/Constants/Constants.js
@@ -0,0 +1,1 @@
+export const CURRENCIES = ["AUD", "BRL", "CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "IDR", "INR", "JPY", "KRW", "MXN", "RUB"];
---
+++ js/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#03A9F4',
+    },
+    title: {
+        height: 30,
+        marginTop: 5,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+});
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            language: null,
+        }
+    }
+
+    _renderPicker() {
+        return (
+            <View>
+                <Text
+                style={styles.title}>Coin-Checker</Text>
+            </View>
+        )
+    }
+
+    _renderHeader() {
+        return (
+            <View style={styles.container}>
+                { this._renderPicker() }
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <View>
+                { this._renderHeader()}
+            </View>
+        );
+    }
+}
+
