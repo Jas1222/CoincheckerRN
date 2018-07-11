0aba255680a9d593689f9f96e57f41ab3e4b2479
Jaspal Hayer <jhayer36@gmail.com>
2018-07-11 11:12:57 +0100

    revert style change

--- js/components/CoinCell/CoinCell.js
+++ js/components/CoinCell/CoinCell.js
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
         fontSize: 22,
         fontWeight: 'bold',
-        marginRight: 5
+        marginRight: 15
     },
     positivePercent: {
         color: '#3D9970',
