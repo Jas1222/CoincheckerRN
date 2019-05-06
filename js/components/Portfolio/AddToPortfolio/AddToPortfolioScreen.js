5778a0838eb3b280383d51dc132f6b73e2c5ebc7
Jaspal Hayer <jhayer36@gmail.com>
2019-05-06 14:45:33 +0100

    Working adding method

--- js/components/Portfolio/AddToPortfolio/AddToPortfolioScreen.js
+++ js/components/Portfolio/AddToPortfolio/AddToPortfolioScreen.js
@@ -6,35 +6,80 @@
 import React from 'react';
 import {
     View,
-    Text,
-    FlatList,
-    TouchableOpacity,
-    Platform
+    FlatList
 } from 'react-native';
 import { connect } from 'react-redux';
-import { styles } from 'DisplayPortfolioScreenStyles';
 import AddCoinCell from 'AddCoinCell';
+import SearchBar from 'SearchBar';
+import EditPortfolioItemComponent from 'EditPortfolioItemComponent';
+import Modal from 'react-native-modal';
 
 export class AddToPortfolioScreen extends React.PureComponent {
     static navigationOptions = {
-        header: null
+        title: 'Add Coins To Portfolio',
+        headerStyle: {
+            backgroundColor: '#03A9F4'
+        },
+        headerTintColor: '#fff'
     };
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            coinData: null,
+            showModal: false,
+            coinToAdd: null
+        }
+
     }
+
+    componentDidMount() {
+        this.setState({ coinData: this.props.coinData })
+    }
+
+    toggleModal = () => {
+        this.setState({
+            showModal: false,
+            editMode: false
+        })
+    }
+
+    onSearchWordEntered = (keyword) => {
+        const data = this.props.coinData;
+
+        const result = data.filter(coin => {
+            const name = coin.name.toLowerCase();
+            const lowercaseKeyword = keyword.toLowerCase();
+
+            return name.includes(lowercaseKeyword);
+        });
+
+
+        this.setState({ coinData: result })
+    }
+
+    onAddPressed = (coinToAdd) => {
+        this.setState({
+            showModal: true,
+            coinToAdd
+        });
+    }
+
 
     renderHeader = () => {
         return (
-            <View>
-
-            </View>
+            <SearchBar onSearchWordEntered={this.onSearchWordEntered} />
         )
     }
 
+
     renderRow = (data) => {
         return (
-           <AddCoinCell item={data.item} />
+            <AddCoinCell
+                item={data.item}
+                onPress={this.onAddPressed}
+            />
         )
     }
 
@@ -52,13 +97,28 @@ export class AddToPortfolioScreen extends React.PureComponent {
 
     render() {
         return (
-            <FlatList
-                data={this.props.coinData}
-                renderItem={this.renderRow}
-                ListHeaderComponent={this.renderHeader()}
-                ItemSeparatorComponent={this.renderSeparator}
-                keyExtractor={item => item.name}
-            />
+            <View>
+                <FlatList
+                    data={this.state.coinData}
+                    renderItem={this.renderRow}
+                    ListHeaderComponent={this.renderHeader()}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    keyExtractor={item => item.name}
+                />
+
+                <Modal
+                    isVisible={this.state.showModal}
+                    animationIn={'pulse'}
+                    onBackdropPress={() => { this.toggleModal }}
+                >
+                    <EditPortfolioItemComponent
+                        item={this.state.coinToAdd}
+                        showModal={this.state.showModal}
+                        expandedOptions={false}
+                        toggleModal={this.toggleModal}
+                    />
+                </Modal>
+            </View >
         )
     }
 }
--- js/components/Portfolio/AddToPortfolio/components/AddCoinCell.js
+++ js/components/Portfolio/AddToPortfolio/components/AddCoinCell.js
@@ -3,51 +3,19 @@
  * @flow
  */
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-const styles = {
-    primaryContainer: {
-        flex: 1,
-        backgroundColor: '#FFFF',
-        padding: 9,
-        flexDirection: 'column'
-    },
-    secondaryContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    columnContainer: {
-        flexDirection: 'column'
-    },
-    primaryText: {
-        color: '#000000',
-        fontSize: 16,
-        marginBottom: 3
-    },
-    secondaryText: {
-        fontSize: 13,
-        justifyContent: 'flex-start',
-    },
-    addButton: {
-        fontWeight: 'bold',
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#03A9F4',
-        color: '#03A9F4',
-        padding: 5
-    },
-    dropdownContainer: {
-        height: 40,
-        backgroundColor: 'red'
-    },
-
-}
+import { styles } from 'AddCoinCellStyles';
+import {
+    TextInput,
+    Text,
+    View,
+    TouchableOpacity
+} from 'react-native';
 
 export default class AddCoinCell extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            enabled: false
+            showModal: false
         }
     }
 
@@ -58,18 +26,13 @@ export default class AddCoinCell extends React.PureComponent {
                     <View style={styles.columnContainer}>
                         <Text style={styles.primaryText}>{this.props.item.name}</Text>
                         <Text style={styles.secondaryText}> {`(${this.props.item.symbol})`}</Text>
-
                     </View>
 
                     <View>
-                        <TouchableOpacity onPress={() => { this.setState({ enabled: true }) }}>
+                        <TouchableOpacity onPress={() => this.props.onPress(this.props.item)}>
                             <Text style={styles.addButton}>{"ADD"}</Text>
                         </TouchableOpacity>
                     </View>
-                </View>
-
-                <View style={styles.dropdownContainer}>
-
                 </View>
             </View>
         );
